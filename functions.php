d48fc3a09101ed4c1f661cf7b1b226e20ddfa999
mateuswetah <mateus.m.luna@gmail.com>
2022-09-28 15:50:42 -0300

    Basic child theme files.

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+if (! defined('WP_DEBUG')) {
+	die( 'Direct access forbidden.' );
+}
+add_action( 'wp_enqueue_scripts', function () {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style('blocksy-child-style', get_stylesheet_uri());
+});
